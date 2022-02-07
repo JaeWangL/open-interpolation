5655e6aa12bbeda05219a6114fdb9d53da4cf17e
JaeWangL <jnso5072@outlook.kr>
2022-02-07 12:27:42 +0900

    [Fix] Change 'uvicorn.run' reload to False

--- main.py
+++ main.py
@@ -47,4 +47,4 @@ def custom_openapi():
 app.openapi = custom_openapi
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='0.0.0.0', port=8080, reload=True)
+    uvicorn.run(app, host='0.0.0.0', port=8080, reload=False)
